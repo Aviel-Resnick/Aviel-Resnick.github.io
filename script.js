c9bf6272c7727a4b6b36fddf4fc860e1c2ff458b
Aviel Resnick <aviel.resnick@gmail.com>
2016-05-19 23:00:07 -0400

    Script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById("nav01").innerHTML =
 "<ul id='menu'>" +
 "<li><a href='index.html'>Home</a></li>" +
-"<li><a href='customers.html'>Projects</a></li>" +
-"<li><a href='about.html'>Contact</a></li>" +
+"<li><a href='projects.html'>Projects</a></li>" +
+"<li><a href='contact.html'>Contact</a></li>" +
 "</ul>";
