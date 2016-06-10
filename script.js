eaed89e5094d6cb32fcf9282084baf5a0e515afa
Aviel Resnick <aviel.resnick@gmail.com>
2016-06-10 09:51:58 -0400

    MSND-R

--- script.js
+++ script.js
@@ -4,4 +4,5 @@ document.getElementById("nav01").innerHTML =
 "<li><a href='projects.html'>Projects</a></li>" +
 "<li><a href='contact.html'>Contact</a></li>" +
 "<li><a href='comment.html'>Comments</a></li>" +
+"<li><a href='Midsummer Night's Dream - Remastered.html'>Midsummer Night's Dream - Remastered</a></li>" +
 "</ul>";
