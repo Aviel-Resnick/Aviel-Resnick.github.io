43436f58f88e8db93252e7bb152522ce3c2051e4
Aviel Resnick <aviel.resnick@gmail.com>
2016-08-13 18:06:29 -0400

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("nav01").innerHTML =
-"<ul id='menu'>" +
-"<li><a href='index.html'>Home</a></li>" +
-"<li><a href='projects.html'>Projects</a></li>" +
-"<li><a href='contact.html'>Contact</a></li>" +
-"<li><a href='MSNDR.html'>Midsummer Night's Dream - Remastered</a></li>" +
-"</ul>";
